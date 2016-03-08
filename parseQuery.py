1975b50dca9fce9401e17636085b1f7c6cb88663
abuckfire <bananabelee@yahoo.com>
2016-03-08 14:40:47 -0600

    Update parseQuery.py

--- parseQuery.py
+++ parseQuery.py
@@ -13,25 +13,25 @@ def get_queries():
 	if len(sys.argv) > 1:
 		queries = str(sys.argv[1:])
 	else:
-		f = open('query_list.txt','rU')
+		f = open('query_list.txt','r')
 		queries = []
 		for row in f:
 			queries.append(row.replace('\n',''))
 		f.close()
 	return queries
 
+query_stems = []
+#keywords are numbers, adjectives, nouns, and verbs... https://cs.nyu.edu/grishman/jet/guide/PennPOS.html	
+keyword_POS=['CD','JJ','JJR','JJS','NN','NNS','NNP','NNPS','VB','VBD','VBG','VBN','VBP','VBZ',]
+
 #takes in a query as a string and returns keywords
 def parseQuery(queries):
-	#keywords are numbers, adjectives, nouns, and verbs... https://cs.nyu.edu/grishman/jet/guide/PennPOS.html
-	keyword_POS=['CD','JJ','JJR','JJS','NN','NNS','NNP','NNPS','VB','VBD','VBG','VBN','VBP','VBZ',]
-	query_stems = []
-	
 	for q in queries:
 		q = tb(q)
 		qtags=q.tags
 		query_stems=[w[0].encode('utf-8') for w in qtags if w[1] in keyword_POS]
 		#q_stemmed=[(PorterStemmer().stem_word(w[0].encode('utf-8')),w[1]) for w in qtags]
-		#query_stems.append(q_stemmed)
+		query_stems.append(query_stems)
 	return query_stems
 
 def main():
